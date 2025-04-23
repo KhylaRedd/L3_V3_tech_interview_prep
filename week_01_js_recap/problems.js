28b78b61a0477e763fc292a6399822bf10baa488
KhylaRedd <kreddick@pursuit.org>
2025-04-23 15:01:12 -0400

    Solved Function

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -9,14 +9,29 @@ reverseString("Hello")
   
 // Find max in array
 function findMax(arr) {
-    let sortedArr = arr.sort((a,b)=> a - b)
-    return sortedArr.length - 1;
+    let sortedArr = arr.sort((a,b)=> a - b);
+    let max =sortedArr[sortedArr.length - 1];
+    return max;
 
 }
 findMax([10,3,2,5,67])
 
 // Check if palindrome
 function isPalindrome(word) {
+    
+
+  let left = 0
+  let right = word.length -1
+  //declare variables that hold numbers to be used as index indicators
+
+  while(left < right){
+    if(word[left] !== word[right]){
+        return false;
+    }
+    left++;
+    right--;
+  }
+    return true;
     
 }
 
