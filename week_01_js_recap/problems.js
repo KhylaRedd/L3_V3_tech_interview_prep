321e01bd64262d84faecfdc5b41c82a463ba77d6
KhylaRedd <kreddick@pursuit.org>
2025-04-23 13:59:38 -0400

    initial Commit

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,12 +1,19 @@
 // Reverse a string
 function reverseString(str) {
+     
 
+    let reversed = str.split('').reverse().join('')
+    return reversed;
 }
+reverseString("Hello")
   
 // Find max in array
 function findMax(arr) {
+    let sortedArr = arr.sort((a,b)=> a - b)
+    return sortedArr.length - 1;
 
 }
+findMax([10,3,2,5,67])
 
 // Check if palindrome
 function isPalindrome(word) {
